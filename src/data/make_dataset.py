005ebc3d6a0ccaad5210bc5c5a5065e0660bc72c
FelipeAMiehrig <felipe.miehrig@gmail.com>
2023-01-12 17:50:59 +0100

    adding initial version of make_dataset.py and train_model.py files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,13 +7,68 @@ from dotenv import find_dotenv, load_dotenv
 
 import os
 import pandas as pd
+import numpy as np
 import torch
+import re
+from transformers import AutoTokenizer
+from datasets import load_dataset, Dataset, DatasetDict, ClassLabel, Value
+
+
+MODEL_CKPT =  "distilbert-base-uncased"
+SAMPLE_SIZE = 10000
+OUT_FILE = '/preprocessed'
 
 class ReviewDataset:
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_CKPT)
+
+    @classmethod
+    def tokenize(cls, batch):
+        return ReviewDataset.tokenizer(batch['review_text'], padding=True, truncation=True)
+
+
     def __init__(self, in_folder: str = '', out_folder: str = ''):
         self.in_folder = in_folder
         self.out_folder = out_folder
-        self.df= pd.read_csv(in_folder +'\dataset.csv')
+        self.df= pd.read_csv(in_folder +'\dataset.csv', usecols = ['review_text', 'review_score'], nrows = SAMPLE_SIZE)
+
+        #preprocessing dataset in pandas
+        self.df.review_text = self.df.review_text.astype(str)
+        self.df = self.df[self.df['review_score'].notnull()]
+        self.df['review_text'] = self.df['review_text'].apply(lambda x: x.strip())
+        self.df["review_score"] = np.where(self.df["review_score"]==-1, 0, self.df["review_score"])
+        self.df = self.df[self.df.review_text != "Early Access Review"]
+        self.df = self.df[~self.df.review_text.isin(['nan'])]
+        self.df['review_text'] = self.df['review_text'].apply(lambda x: re.sub(r"[♥]+", ' **** ' ,x))
+
+
+        #converting to Dataset
+        self.ds = Dataset.from_pandas(self.df)
+        new_features = self.ds.features.copy()
+        new_features["review_score"] = ClassLabel(names=[0, 1])
+        self.ds = self.ds.cast(new_features)
+
+        # 90% train, 10% test + validation
+        train_testvalid = self.ds.train_test_split(test_size=0.4)
+        # Split the 10% test + valid in half test, half valid
+        test_valid = train_testvalid['test'].train_test_split(test_size=0.5)
+        # gather everyone if you want to have a single DatasetDict
+        self.processed = DatasetDict({
+            'train': train_testvalid['train'],
+            'test': test_valid['test'],
+            'valid': test_valid['train']})  
+        print(self.processed)
+        #self.ds = self.ds.train_test_split(test_size=0.2)
+        self.processed = self.processed.map(ReviewDataset.tokenize, batched=True, batch_size=None)
+        print(self.processed)
+        #print(self.processed["train"].column_names)
+
+        self.processed.save_to_disk(self.out_folder)
+        #self.processed.to_csv(self.out_folder)
+
+
+    
+
     
 
 @click.command()
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,57 @@
 import torch
-import pandas as pd
+from datasets import load_dataset, Dataset, DatasetDict, ClassLabel, Value, load_from_disk
+
+
 from src.data.make_dataset import ReviewDataset
+
+from transformers import AutoModelForSequenceClassification,Trainer, TrainingArguments, AutoTokenizer
+from sklearn.metrics import accuracy_score, f1_score
+
+
+MODEL_CKPT =  "distilbert-base-uncased"
+
 
 dataset = ReviewDataset('data/raw','data/processed')
 
-print(dataset.df.head())
+emotions_encoded = load_from_disk('data/processed')
+
+print(emotions_encoded['train'][:1])
+
+num_labels = 2
+
+model = (AutoModelForSequenceClassification
+         .from_pretrained(MODEL_CKPT, num_labels=num_labels))
+         #.to(device))
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_CKPT)
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    f1 = f1_score(labels, preds, average="weighted")
+    acc = accuracy_score(labels, preds)
+    return {"accuracy": acc, "f1": f1}
+
+batch_size = 64
+logging_steps = len(emotions_encoded["train"]) // batch_size
+model_name = f"{MODEL_CKPT}-finetuned-emotion"
+training_args = TrainingArguments(output_dir=model_name,
+                                  num_train_epochs=2,
+                                  learning_rate=2e-5,
+                                  per_device_train_batch_size=batch_size,
+                                  per_device_eval_batch_size=batch_size,
+                                  weight_decay=0.01,
+                                  evaluation_strategy="epoch",
+                                  disable_tqdm=False,
+                                  logging_steps=logging_steps,
+                                  push_to_hub=False, 
+                                  log_level="error")
+                                  
+trainer = Trainer(model=model, args=training_args, 
+                  compute_metrics=compute_metrics,
+                  train_dataset=emotions_encoded["train"],
+                  eval_dataset=emotions_encoded["valid"],
+                  tokenizer=tokenizer)
+trainer.train()
+
+
