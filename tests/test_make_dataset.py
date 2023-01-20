e2125c0b245caa36581e020bbbe1fe8f6751c6f7
FelipeAMiehrig <felipe.miehrig@gmail.com>
2023-01-20 10:06:32 +0100

    adding new tests

--- tests/test_make_dataset.py
+++ tests/test_make_dataset.py
@@ -1,38 +1,52 @@
-from tests import _PATH_DATA
 import os.path
 import pytest
+from tests import _PATH_DATA
 from src.data.make_dataset import ReviewDataset
 
-sample_size = 1000
-num_labels = 2
-dataset = ReviewDataset(in_folder='data/raw', out_folder='data/processed',
-    name='distilbert-base-uncased', sample_size=sample_size, force=True)
-dataset = dataset.processed
+
 
 def test_something_two():
     assert True
 
 @pytest.mark.skipif(not os.path.exists("data/raw"), reason="Data files not found")
-def test_train_test_size(dataset=dataset):
-
+def test_train_test_size():
+    sample_size = 1000
+    
+    dataset = ReviewDataset(in_folder='data/raw', out_folder='data/processed',
+    name='distilbert-base-uncased', sample_size=sample_size, force=True)
+    dataset = dataset.processed 
     size = len(dataset['train']) + len(dataset['test']) + len(dataset['valid'])
     assert size == sample_size
 
 @pytest.mark.skipif(not os.path.exists("data/raw"), reason="Data files not found")
-def test_dataset_columns(dataset=dataset):
+def test_dataset_columns():
+    sample_size = 1000
 
+    dataset = ReviewDataset(in_folder='data/raw', out_folder='data/processed',
+    name='distilbert-base-uncased', sample_size=sample_size, force=True)
+    dataset = dataset.processed 
     columns = set( ['label', 'input_ids', 'attention_mask', 'text'])
     bool_list = [columns == set(dataset[x].column_names) for x in ['train', 'test', 'valid']]
     assert all(bool_list)
 
 @pytest.mark.skipif(not os.path.exists("data/raw"), reason="Data files not found")
-def test_n_labels(dataset=dataset, num_labels=num_labels):
+def test_n_labels():
+    sample_size = 1000
 
+    dataset = ReviewDataset(in_folder='data/raw', out_folder='data/processed',
+    name='distilbert-base-uncased', sample_size=sample_size, force=True)
+    dataset = dataset.processed 
+    num_labels=2
     total_labels = len(set(dataset['train']['label']))
     assert total_labels == num_labels
 
 @pytest.mark.skipif(not os.path.exists("data/raw"), reason="Data files not found")
-def test_all_labels_train_test(dataset=dataset, num_labels=num_labels):
+def test_all_labels_train_test():
+    sample_size = 1000
 
+    dataset = ReviewDataset(in_folder='data/raw', out_folder='data/processed',
+    name='distilbert-base-uncased', sample_size=sample_size, force=True)
+    dataset = dataset.processed 
+    num_labels=2
     bool_list = [num_labels == len(set(dataset[x]['label'])) for x in ['train', 'test', 'valid']]
     assert all(bool_list)
